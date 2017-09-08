8857a463c6ae776c8312e2424343c61344995d3f
Salvador Villegas <villegas@berkeley.edu>
2017-09-08 19:52:06 +0000

    Passing every scenario and prevented one form of cheating

--- app.rb
+++ app.rb
@@ -29,9 +29,9 @@ class HangpersonApp < Sinatra::Base
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || HangpersonGame.get_random_word
     # NOTE: don't change previous line - it's needed by autograder!
+    
 
     @game = HangpersonGame.new(word)
-    session[:game] = @game
     redirect '/show'
   end
   
@@ -41,7 +41,8 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game = session[:game]
+    #byebug
+    #@game = session[:game]
     begin
       if @game.guess letter
         session[:game] = @game
@@ -65,17 +66,34 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    @game = session[:game]
+    win = @game.check_win_or_lose
+    if win == :win
+      redirect "/win"
+    elsif win == :lose
+      redirect "/lose"
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    @game = session[:game]
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      redirect "/show"
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    @game = session[:game]
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect "/show"
+    end
   end
   
 end
